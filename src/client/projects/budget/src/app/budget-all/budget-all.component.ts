65a2a3627be339f78455882b389ee7cb4f67257b
briansain <brian.sain@yahoo.com>
2019-05-18 21:42:26 -0500

    clean up and add paycheck class

--- src/client/projects/budget/src/app/budget-all/budget-all.component.ts
+++ src/client/projects/budget/src/app/budget-all/budget-all.component.ts
@@ -44,16 +44,16 @@ export class BudgetAllComponent implements OnInit {
     }]
   };
 
-  editPaycheck = false;
+  editPaycheck = new Paycheck;
 
-  paychecks = [{
+  paychecks: Paycheck[] = [{
     source: "Job 1",
-    amount: "1800",
+    amount: 1800,
     startDate: new Date(2019, 5, 1),
     endDate: new Date(2019, 5, 14)
   },{
     source: "Job 2",
-    amount: "1800",
+    amount: 1800,
     startDate: new Date(2019, 5, 15),
     endDate: new Date(2019, 5, 31)
   }];
@@ -64,4 +64,11 @@ export class BudgetAllComponent implements OnInit {
   ngOnInit() {
   }
 
+}
+
+export class Paycheck {
+  source = "";
+  amount = 0;
+  startDate = new Date();
+  endDate = new Date();
 }
